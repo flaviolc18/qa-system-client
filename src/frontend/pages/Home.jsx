a01c8c0d03335b9fa3575493b16fa25c88ee2b1b
Marcos Vinicius <42280870+Marcos30004347@users.noreply.github.com>
2019-04-20 18:05:02 -0300

    feat(components): feed para centralizar os elementos da página  (#8)

--- src/frontend/pages/Home.jsx
+++ src/frontend/pages/Home.jsx
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div className="teste">UFMG QeA</div>;
+    return (
+      <div className="teste">
+        <div className="row justify-content-center">
+          <h1>Página inicial</h1>
+        </div>
+      </div>
+    );
   }
 }
 
