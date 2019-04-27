26207fe058f97c22babc1694326655d4b64ff34c
Marcos <marcos30004347@gmail.com>
2019-04-27 02:11:48 -0300

    _m

--- src/frontend/components/FullPage.jsx
+++ src/frontend/components/FullPage.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Feed from './Feed';
 import PropTypes from 'prop-types';
+import { http } from '../helpers/http';
 
-const links = [
-  { label: 'Home', class: 'navigation', type: 'link', to: '/home' },
+const sharedLink = [{ label: 'Home', class: 'navigation', type: 'link', to: '/home' }];
+const unloggedLinks = sharedLink.concat([
   { label: 'Login', class: 'navigation', type: 'link', to: '/login' },
   { label: 'Registrar-se', class: 'navigation', type: 'link', to: '/registro-usuario' },
+]);
+const loggedLink = sharedLink.concat([
   { label: 'Postar Pergunta', class: 'navigation', type: 'link', to: '/postar-pergunta' },
-
   {
     label: (
       <div>
@@ -21,18 +23,57 @@ const links = [
     type: 'button',
     action: () => alert('Logout'),
   },
-  { label: 'Dropdown', class: 'navigation', type: 'dropdown', links: [{ label: 'Teste', to: 'Teste' }] },
-];
+]);
+
+const AppContext = React.createContext({
+  isLogged: false,
+  usuarioId: '',
+});
 
 class FullPage extends Component {
-  componentDidUpdate() {
-    console.log('update');
+  constructor(props) {
+    super(props);
+    this.state = { isLogged: false, usuarioId: '' };
+    this.getUserState = this.getUserState.bind(this);
   }
+
+  getUserState() {
+    http.get('/api/sessions').then(session => {
+      if (session && session.usuarioId)
+        if (session.usuarioId != this.state.usuarioId) {
+          this.setState({ isLogged: true, usuarioId: session.usuarioId });
+        }
+    });
+  }
+
+  componentDidMount() {
+    this.getUserState();
+  }
+
+  componentDidUpdate() {
+    this.getUserState();
+  }
+
   render() {
+    const value = this.state;
+
     return (
       <div>
-        <Navbar to="home" title={'Não Faço a Menor Ideia'} links={links} />
-        <Feed>{this.props.children}</Feed>
+        <AppContext.Provider value={value}>
+          <AppContext.Consumer>
+            {value => (
+              <div>
+                <Navbar
+                  context={value}
+                  to="/home"
+                  title={'Não Faço a Menor Ideia'}
+                  links={this.state.isLogged ? loggedLink : unloggedLinks}
+                />
+                <Feed context={value}>{this.props.children}</Feed>
+              </div>
+            )}
+          </AppContext.Consumer>
+        </AppContext.Provider>
       </div>
     );
   }
--- src/frontend/components/Navbar.jsx
+++ src/frontend/components/Navbar.jsx
@@ -49,9 +49,9 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to={this.props.to}>
           {this.props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
