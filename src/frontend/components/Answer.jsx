1ab964ea14b268ef1d580930e4486206ea290035
Marcos <marcos30004347@gmail.com>
2019-05-22 21:55:24 -0300

    _m

--- src/frontend/components/Answer.jsx
+++ src/frontend/components/Answer.jsx
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { connect } from 'react-redux';
+import { downvoteResposta, editResposta, removeResposta, upvoteResposta } from '../redux/respostas.redux';
 import Post from './Post';
 
 class Answer extends Component {
   render() {
     if (!this.props.user || !this.props.resposta) {
-      return '';
+      return 'Loading...';
     }
     return (
-      <div className="row p-3">
-        <div className="p-3" style={{ width: '100%', borderRadius: '5px', backgroundColor: 'rgb(245,245,245)' }}>
-          <Post post={this.props.resposta} user={this.props.user} />
-        </div>
-      </div>
+      <Post
+        removePost={this.props.removeResposta}
+        editPost={this.props.editResposta}
+        upVote={this.props.upvoteResposta}
+        downVote={this.props.downvoteResposta}
+        votes={{ upvotes: this.props.resposta.upvotes, downvotes: this.props.resposta.downvotes }}
+        post={this.props.resposta}
+        user={this.props.user}
+      />
     );
   }
 }
 
 Answer.propTypes = {
+  removeResposta: PropTypes.func,
+  editResposta: PropTypes.func,
+  upvoteResposta: PropTypes.func,
+  downvoteResposta: PropTypes.func,
   resposta: PropTypes.object,
   user: PropTypes.object,
 };
 
-export default Answer;
+export default connect(
+  () => ({}),
+  { removeResposta, editResposta, upvoteResposta, downvoteResposta }
+)(Answer);
