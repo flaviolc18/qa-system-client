5ebe5d5c338968b75e7696fa16b68aee7f85008b
Marcos <marcos30004347@gmail.com>
2019-05-26 22:15:40 -0300

    _m

--- src/frontend/redux/perguntas.redux.js
+++ src/frontend/redux/perguntas.redux.js
@@ -12,18 +12,7 @@ let deletePerguntaURL = filters => '/api/perguntas/' + filters.id;
 let actions = actionsFactory({
   context,
   buildURLs: {
-<<<<<<< HEAD
-    loadOneURLs: [
-      loadPerguntaURL,
-      loadPerguntasUsuarioURL,
-      loadPerguntasURL,
-      upvotePerguntaURL,
-      downvotePerguntaURL,
-      searchPerguntaURL,
-    ],
-=======
     loadOneURLs: [loadPerguntaURL, loadPerguntasUsuarioURL, loadPerguntasURL],
->>>>>>> d9f96811ff7547a3dac090e12b1859d7ec5da698
     createOneURLs: [postPerguntaURL],
     editOneURLs: [editPerguntaURL],
     removeOneURLs: [deletePerguntaURL],
@@ -31,17 +20,6 @@ let actions = actionsFactory({
 });
 
 let getters = gettersFactory({ context });
-<<<<<<< HEAD
-export const editPergunta = actions.edit[0];
-export const loadPerguntasUsuario = actions.load[1];
-export const loadPerguntas = actions.load[2];
-export const upvotePergunta = actions.load[3];
-export const downvotePergunta = actions.load[4];
-export const searchPergunta = actions.load[5];
-
-export const removePergunta = actions.remove[0];
-=======
->>>>>>> d9f96811ff7547a3dac090e12b1859d7ec5da698
 
 export const getPergunta = getters.getOneById;
 export const getPerguntaByFilters = getters.getByFilters;
