a85e895aa28f81f96865eae92ce32731723b4b27
Flavio Lucio <flaviolc18@gmail.com>
2019-05-31 00:15:54 -0300

    fix(usuario): bloqueia alteração de senha pela api update

--- src/backend/api/usuario/change-password-usuario.js
+++ src/backend/api/usuario/change-password-usuario.js
@@ -1,23 +1,24 @@
 'use strict';
 
 module.exports = async function(fastify) {
-  fastify.post('/usuarios/change-password/:usuarioId', {}, async function({
+  fastify.post('/usuarios/change-password/:usuarioId', async function({
     params: { usuarioId },
     body: { password, newPassword },
   }) {
-    try {
-      const usuario = await fastify.core.models.usuario.find({ _id: usuarioId });
-      const isValid = await usuario.comparePassword(password);
+    const usuario = await fastify.core.models.usuario.find({ _id: usuarioId });
 
-      if (!isValid) {
-        throw fastify.httpErrors.unauthorized();
-      }
-
-      const updatedUsuario = await fastify.core.models.usuario.update({ _id: usuarioId }, { password: newPassword });
-
-      return fastify.getResponseObject(updatedUsuario);
-    } catch ({ message }) {
+    if (!usuario) {
       throw fastify.httpErrors.notFound();
     }
+
+    const isValid = await usuario.comparePassword(password);
+
+    if (!isValid) {
+      throw fastify.httpErrors.unauthorized('Senha incorreta');
+    }
+
+    const updatedUsuario = await fastify.core.models.usuario.update({ _id: usuarioId }, { password: newPassword });
+
+    return fastify.getResponseObject(updatedUsuario);
   });
 };
