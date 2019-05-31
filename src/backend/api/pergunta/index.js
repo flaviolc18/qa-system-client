396ed72f05ab9d52c0d7081571003fe9a39f98b1
Flavio Lucio <flaviolc18@gmail.com>
2019-05-30 23:34:08 -0300

    feat(perguntas): api para pesquisa (#38)
    * feat(tags): entidade e regra de negócio
    
    * aa
    
    * altera imports
    
    * feat(perguntas): api para pesquisa

--- src/backend/api/pergunta/index.js
+++ src/backend/api/pergunta/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = async function(fastify) {
-  fastify.register(require('./find-all-perguntas'));
+  fastify.register(require('./search-perguntas'));
   fastify.register(require('./find-perguntas-usuario'));
   fastify.register(require('./create-pergunta'));
   fastify.register(require('./find-pergunta'));
--- test/utils/unit/serializer.test.js
+++ test/utils/unit/serializer.test.js
@@ -10,13 +10,9 @@ test('serialize null', async t => {
 
 test('serialize null', async t => {
   t.same(serialize({ teste: 'a' }), 'teste=a');
-  t.same(serialize({ teste: 'a', b: { c: 'd' } }), 'teste=a&b%5Bc%5D=d');
   t.same(serialize({ teste1: 'a', teste2: 'b' }), 'teste1=a&teste2=b');
-  t.same(serialize({ teste1: 'a', teste2: 'b' }, 'lala'), 'lala%5Bteste1%5D=a&lala%5Bteste2%5D=b');
-  t.same(
-    serialize({ teste1: 'a', teste2: 'b', a: null }, 'lala'),
-    'lala%5Bteste1%5D=a&lala%5Bteste2%5D=b&lala%5Ba%5D=null'
-  );
+  t.same(serialize({ teste1: 'a', teste2: 'b' }, 'lala'), 'lala=a&lala=b');
+  t.same(serialize({ teste1: 'a', teste2: 'b', a: null }, 'lala'), 'lala=a&lala=b&lala=null');
 
   t.end();
 });
