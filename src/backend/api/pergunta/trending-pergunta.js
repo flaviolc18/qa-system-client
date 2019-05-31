e9800ab68bf941bdbb5cbd160411caf4401afb23
Flavio Lucio <flaviolc18@gmail.com>
2019-05-31 12:51:33 -0300

    feat(trending-pergunta): skip & limit

--- src/backend/api/pergunta/trending-pergunta.js
+++ src/backend/api/pergunta/trending-pergunta.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const schema = { querystring: { skip: { type: 'integer' }, limit: { type: 'integer' } } };
+
 module.exports = async function(fastify) {
-  fastify.get('/perguntas/trending', async function() {
+  fastify.get('/perguntas/trending', { schema }, async function({ query: { skip = 0, limit = 10 } }) {
     const yesterday = new Date();
 
     yesterday.setDate(new Date().getDate() - 1);
@@ -10,6 +12,8 @@ module.exports = async function(fastify) {
       { $match: { dataCriacao: { $gte: yesterday } } },
       { $addFields: { sort_order: { $add: ['$upvotes', '$downvotes'] } } },
       { $sort: { sort_order: -1 } },
+      { $skip: skip },
+      { $limit: limit },
       { $project: { sort_order: 0 } },
     ];
 
