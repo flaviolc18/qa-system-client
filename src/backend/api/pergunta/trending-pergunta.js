519e7bdbe561b3187b77b7ce3897211c60687e3c
Flavio Lucio <flaviolc18@gmail.com>
2019-05-30 23:54:04 -0300

    feat(perguntas): api trending

--- src/backend/api/pergunta/trending-pergunta.js
+++ src/backend/api/pergunta/trending-pergunta.js
@@ -2,9 +2,18 @@
 
 module.exports = async function(fastify) {
   fastify.get('/perguntas/trending', async function() {
-    const yesterday = new Date().setDate(new Date().getDate() - 1);
+    const yesterday = new Date();
 
-    const perguntas = await fastify.core.models.pergunta.findAll({ dataCriacao: { $gt: yesterday } });
+    yesterday.setDate(new Date().getDate() - 1);
+
+    const pipeline = [
+      { $match: { dataCriacao: { $gte: yesterday } } },
+      { $addFields: { sort_order: { $add: ['$upvotes', '$downvotes'] } } },
+      { $sort: { sort_order: -1 } },
+      { $project: { sort_order: 0 } },
+    ];
+
+    const perguntas = await fastify.core.models.pergunta.aggregate(pipeline);
 
     return fastify.getResponseObject(perguntas);
   });
--- src/core/models/pergunta/index.js
+++ src/core/models/pergunta/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  aggregate: require('./aggregate-perguntas'),
   create: require('./create-pergunta'),
   delete: require('./delete-pergunta'),
   findAll: require('./find-all-perguntas'),
